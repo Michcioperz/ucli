a016595685e0bdd9a88c647e0b9fc05c3ac12003
teqwve <teqwve@openmailbox.org>
2015-10-08 00:07:24 +0200

    more options, plan future work

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from sys import exit
-from src.arguments import get_parser
+from . import arguments
 
 def main():
-    args = get_parser().parse_args()
+    args = arguments.get_parser().parse_args()
 
     if args.version:
         print ("no version yet")
         exit(0)
+
+    if "func" in vars(args).keys():
+        args.func(args)
